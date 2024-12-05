51a64df1bebb73e19f75272599199097b605813e
권진우 <rnjswlsdn3452@gmail.com>
2024-12-05 17:30:57 +0900

    회원정보 수정 API에 관한 어노테이션 추가

--- src/main/java/com/example/saramin/controller/authentication/AuthenticationControllerDocs.java
+++ src/main/java/com/example/saramin/controller/authentication/AuthenticationControllerDocs.java
@@ -2,6 +2,7 @@ package com.example.saramin.controller.authentication;
 
 import com.example.saramin.controllerAdvice.CustomExceptions;
 import com.example.saramin.entity.dto.Authentication.LoginForm;
+import com.example.saramin.entity.dto.Authentication.ProfileForm;
 import com.example.saramin.entity.dto.Authentication.RefreshForm;
 import com.example.saramin.entity.dto.Authentication.RegisterForm;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
@@ -20,10 +22,10 @@ public interface AuthenticationControllerDocs {
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "회원가입 성공",
                     content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = "{\"status\": \"success\", \"message\": \"회원가입 성공\"}"))),
+                            examples = @ExampleObject(value = "{\"status\": \"200\", \"message\": \"회원가입 성공\"}"))),
             @ApiResponse(responseCode = "400", description = "회원가입 실패",
                     content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = "{\"status\": \"error\", \"message\": \"회원가입 실패 사유\"}")))
+                            examples = @ExampleObject(value = "{\"status\": \"400\", \"message\": \"회원가입 실패 사유\"}")))
     })
     ResponseEntity<Map<String, Object>> register(@RequestBody RegisterForm registerForm) throws CustomExceptions.InvalidRegisterCredentialsException;
 
@@ -32,31 +34,34 @@ public interface AuthenticationControllerDocs {
             @ApiResponse(responseCode = "200", description = "로그인 성공",
                     content = @Content(mediaType = "application/json",
                             examples = @ExampleObject(value = "{"
-                                    + "\"status\": \"success\", "
+                                    + "\"status\": \"200\", "
+                                    + "\"message\": \"로그인 성공\", "
                                     + "\"user\": {"
                                     + "\"id\": 1, "
                                     + "\"email\": \"test@gmail.com\", "
-                                    + "\"username\": \"홍길동\""
+                                    + "\"username\": \"홍길동\", "
+                                    + "\"phoneNumber\": \"010-1234-5678\""
                                     + "}, "
                                     + "\"jwtToken\": {"
                                     + "\"grantType\": \"Bearer\", "
-                                    + "\"accessToken\": \"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNzMzMzY3NjAyfQ.LE0l8UYIEDh8revE-XCW1C9AoBbZCNCip9XhhhP7OpY\", "
-                                    + "\"refreshToken\": \"eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzM1Mzg2MDJ9.PCaRodsx8HrXBmDIC_5FqmVUq8c3A3BPOoIVgQ-BVLY\", "
-                                    + "\"expireDate\": \"2024-12-07T02:30:02.374+00:00\""
+                                    + "\"accessToken\": \"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNzMzMzg4MjcxfQ.vUUuDEJdPXprfeyriunwDoBQh8555iXB4mRMafpmAvs\", "
+                                    + "\"refreshToken\": \"eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzM1NTkyNzF9.DX0BxeRtF6ztaCwlmcitzXN_SWB2JLUc9xA85mBP2bc\", "
+                                    + "\"expireDate\": \"2024-12-07T08:14:31.557+00:00\""
                                     + "}"
                                     + "}"))),
             @ApiResponse(responseCode = "403", description = "로그인 실패",
                     content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = "{\"status\": \"error\", \"message\": \"로그인 실패 사유\"}")))
+                            examples = @ExampleObject(value = "{\"status\": \"400\", \"message\": \"로그인 실패 사유\"}")))
     })
     ResponseEntity<Map<String, Object>> login(@RequestBody LoginForm loginForm) throws CustomExceptions.InvalidLoginCredentialsException;
 
-    @Operation(summary = "로그인", description = "사용자의 인증을 위한 로그인 API")
+    @Operation(summary = "Jwt 재발급", description = "Jwt 재발급을 위한 API")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "토큰 재발급 성공",
                     content = @Content(mediaType = "application/json",
                             examples = @ExampleObject(value = "{"
-                                    + "\"status\": \"success\", "
+                                    + "\"status\": \"200\", "
+                                    + "\"message\": \"토큰 재발급 성공\", "
                                     + "\"jwtToken\": {"
                                     + "\"grantType\": \"Bearer\", "
                                     + "\"accessToken\": \"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNzMzMzY3NzQwfQ.HhNAo5NvGRV3Z65DmjONQwNk8p00NnllYJVsxHvO53c\", "
@@ -64,9 +69,32 @@ public interface AuthenticationControllerDocs {
                                     + "\"expireDate\": \"2024-12-07T02:32:20.239+00:00\""
                                     + "}"
                                     + "}"))),
-            @ApiResponse(responseCode = "401", description = "토큰 재발급 실패",
+            @ApiResponse(responseCode = "400", description = "토큰 재발급 실패",
                     content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = "{\"status\": \"error\", \"message\": \"토큰 재발급 실패 사유\"}")))
+                            examples = @ExampleObject(value = "{\"status\": \"400\", \"message\": \"토큰 재발급 실패 사유\"}")))
     })
     ResponseEntity<Map<String, Object>> refresh(@RequestBody RefreshForm refreshForm) throws CustomExceptions.InvalidLoginCredentialsException;
+
+    @Operation(summary = "회원정보 수정", description = "비밀번호 또는 사용자 전화번호를 수정하기 위한 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원정보 수정 성공",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = "{"
+                                    + "\"status\": \"200\", "
+                                    + "\"message\": \"회원정보 수정 성공\", "
+                                    + "\"user\": {"
+                                    + "\"id\": 1, "
+                                    + "\"email\": \"test@gmail.com\", "
+                                    + "\"password\": \"dGVzdDEyMzQ1\", "
+                                    + "\"username\": \"홍길동\", "
+                                    + "\"phoneNumber\": \"010-8765-4321\", "
+                                    + "\"userRole\": \"USER\""
+                                    + "}"
+                                    + "}"))),
+            @ApiResponse(responseCode = "401", description = "회원정보 수정 실패",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\"status\": \"400\", \"message\": \"회원정보 수정 실패 사유\"}")))
+    })
+    ResponseEntity<Map<String, Object>> profile(Authentication auth, @RequestBody ProfileForm profileForm) throws CustomExceptions.InvalidProfileCredentialsException;
+
 }
