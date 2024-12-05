ccc67ea08f2563b62f8e27aa4e1cec8137b101ea
권진우 <rnjswlsdn3452@gmail.com>
2024-12-05 09:09:06 +0900

    사용자 엔티티 구현

--- src/main/java/com/example/saramin/entity/model/User.java
+++ src/main/java/com/example/saramin/entity/model/User.java
@@ -1,4 +1,27 @@
 package com.example.saramin.entity.model;
 
+import com.example.saramin.entity.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    private String username;
+
+    private UserRole userRole;
 }
