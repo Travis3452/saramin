023471e3b04f366c823327622dafece3cd371070
권진우 <rnjswlsdn3452@gmail.com>
2024-12-05 17:29:33 +0900

    적절한 클래스명으로 수정

--- src/main/java/com/example/saramin/util/FormValidator.java
+++ src/main/java/com/example/saramin/util/FormValidator.java
@@ -4,7 +4,7 @@ import com.example.saramin.entity.dto.Authentication.RegisterForm;
 
 import java.util.regex.Pattern;
 
-public class RegisterValidator {
+public class FormValidator {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
     private static final String PHONENUMBER_REGEX = "^010-\\d{4}-\\d{4}$";
