cdb34743b157f8086aec90e62e748d08e1305d39
권진우 <rnjswlsdn3452@gmail.com>
2024-12-05 09:31:46 +0900

    RefreshToken 엔티티 관련 데이터베이스 상호작용 인터페이스 추가

---
+++ src/main/java/com/example/saramin/repository/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.saramin.repository;
+
+import com.example.saramin.entity.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
