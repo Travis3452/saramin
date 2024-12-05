36d26d6f41504ab29417fe2afd292d05bbb613d1
권진우 <rnjswlsdn3452@gmail.com>
2024-12-05 17:27:55 +0900

    회원정보 수정에 필요한 메서드 추가

--- src/main/java/com/example/saramin/entity/model/User.java
+++ src/main/java/com/example/saramin/entity/model/User.java
@@ -2,13 +2,11 @@ package com.example.saramin.entity.model;
 
 import com.example.saramin.entity.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
