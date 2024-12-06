2e688cd551e207cb8e719b2b05c1ee136526fe42
권진우 <rnjswlsdn3452@gmail.com>
2024-12-06 22:00:08 +0900

    글로버 에러 핸들러 구현

--- src/main/java/com/example/saramin/controllerAdvice/GlobalExceptionHandler.java
+++ src/main/java/com/example/saramin/controllerAdvice/GlobalExceptionHandler.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomExceptions.InvalidRegisterCredentialsException.class)
-    public ResponseEntity<String> handleInvalidRegisterCredentialsException(CustomExceptions.InvalidRegisterCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<String> handleInvalidRegisterCredentialsException(CustomExceptions.InvalidRegisterCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(CustomExceptions.InvalidTokenException.class)
-    public ResponseEntity<String> handleInvalidTokenException(CustomExceptions.InvalidTokenException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    public ResponseEntity<String> handleInvalidTokenException(CustomExceptions.InvalidTokenException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler(CustomExceptions.InvalidLoginCredentialsException.class)
-    public ResponseEntity<String> handleInvalidLoginCredentialsException(CustomExceptions.InvalidLoginCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    public ResponseEntity<String> handleInvalidLoginCredentialsException(CustomExceptions.InvalidLoginCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler(CustomExceptions.InvalidProfileCredentialsException.class)
-    public ResponseEntity<String> handleInvalidProfileCredentialsException(CustomExceptions.InvalidProfileCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<String> handleInvalidProfileCredentialsException(CustomExceptions.InvalidProfileCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
