2dc3de4eb6ccd4444d9e7fa5167e1aaff14b4f1c
권진우 <rnjswlsdn3452@gmail.com>
2024-12-06 21:35:51 +0900

    회사 엔티티 추가

--- src/main/java/com/example/saramin/entity/model/Company.java
+++ src/main/java/com/example/saramin/entity/model/Company.java
@@ -1,4 +1,21 @@
 package com.example.saramin.entity.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "company")
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String companyName;
 }
